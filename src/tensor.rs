80e23e946873bdc6ee1fffea4628d7acea0f7a0f
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-02-27 16:15:06 -0800

    Update tensor.rs

--- src/tensor.rs
+++ src/tensor.rs
@@ -1,5 +1,19 @@
 use crate::specs::tensor_spec::TensorSpec;
 
+pub trait TensorLike {
+    fn spec(&self) -> TensorSpec;
+
+    fn dim_sizes(&self) -> Vec<u32> {
+        self.spec().dim_sizes
+    }
+}
+
 pub struct Tile {
-    spec: TensorSpec,
+    _spec: TensorSpec,
+}
+
+impl TensorLike for Tile {
+    fn spec(&self) -> TensorSpec {
+        self._spec.clone()
+    }
 }
