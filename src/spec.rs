380cfc5661c7aab97b82727d85425524499ba206
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-02-28 02:34:48 -0800

    Improve the interface of spec

--- src/spec.rs
+++ src/spec.rs
@@ -1,11 +1,17 @@
 use std::fmt::{Debug, Formatter};
 
-pub(crate) type Spec = MatMul;
+pub type Spec = MatMul;
 
 #[derive(Clone)]
-pub(crate) struct MatMul {
+pub struct MatMul {
     pub(crate) x: usize,
     pub(crate) y: usize,
+}
+
+impl MatMul {
+    pub fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
 }
 
 impl Debug for MatMul {
