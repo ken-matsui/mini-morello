5728e0ad6b2d9c2a2166309ba461c02d12997bde
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-02-27 16:35:56 -0800

    Add tiling & simplify tensor

--- src/tensor.rs
+++ src/tensor.rs
@@ -1,19 +1,5 @@
 use crate::specs::tensor_spec::TensorSpec;
 
-pub trait TensorLike {
-    fn spec(&self) -> TensorSpec;
-
-    fn dim_sizes(&self) -> Vec<u32> {
-        self.spec().dim_sizes
-    }
-}
-
-pub struct Tile {
-    _spec: TensorSpec,
-}
-
-impl TensorLike for Tile {
-    fn spec(&self) -> TensorSpec {
-        self._spec.clone()
-    }
+pub struct SimpleTile {
+    pub spec: TensorSpec,
 }
