45a59684e4517ff7879ab901389d2abaacd1734e
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-02-28 15:13:19 -0800

    Add cost.rs

---
+++ src/cost.rs
@@ -0,0 +1,23 @@
+use crate::dp::Elem;
+use crate::dp_table::DpTablePtr;
+use crate::r#impl::Impl;
+use crate::spec::Spec;
+
+pub(crate) type Cost = i32;
+
+#[inline]
+fn frac(a: usize, b: usize) -> f32 {
+    a as f32 / b as f32
+}
+
+pub(crate) fn cost(imp: Impl, target_spec: Spec, dp: DpTablePtr<Elem>) -> Cost {
+    match imp {
+        Impl::Mult => 1, // Base Case
+        Impl::Loop { child } => {
+            let loop_cost = frac(target_spec.x, child.x) * frac(target_spec.y, child.y);
+            let child_cost = unsafe { dp.get(child.x, child.y) };
+            // loop_cost * child_cost = this impl's cost
+            (loop_cost * child_cost.1 as f32).ceil() as Cost
+        }
+    }
+}
