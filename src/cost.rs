7c11e07e74c94d8c1e6988132ab57411036e64ec
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-02-28 16:02:02 -0800

    Some tweaks on cost and add tests for cost

--- src/cost.rs
+++ src/cost.rs
@@ -7,22 +7,45 @@ use crate::util::dec;
 pub(crate) type Cost = i32;
 
 #[inline]
-fn frac(a: usize, b: usize) -> f32 {
-    a as f32 / b as f32
+fn frac(numerator: usize, denominator: usize) -> f32 {
+    numerator as f32 / denominator as f32
 }
 #[inline]
-fn frac_ceil(a: usize, b: usize) -> i32 {
-    frac(a, b).ceil() as i32
+fn frac_ceil(numerator: usize, denominator: usize) -> i32 {
+    frac(numerator, denominator).ceil() as i32
 }
 
-pub(crate) fn cost(imp: Impl, target_spec: Spec, dp: DpTablePtr<Elem>) -> Cost {
+pub(crate) fn cost(base_spec: Spec, imp: Impl, dp: DpTablePtr<Elem>) -> Cost {
     match imp {
         Impl::Mult => 1, // Base Case
         Impl::Loop { child } => {
-            let loop_cost = frac_ceil(target_spec.x, child.x) * frac_ceil(target_spec.y, child.y);
+            let loop_cost = frac_ceil(base_spec.x, child.x) * frac_ceil(base_spec.y, child.y);
             let child_cost = unsafe { dp.get(dec(child.x), dec(child.y)).1 };
             // loop_cost * child_cost = this impl's cost
             (loop_cost * child_cost) as Cost
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_frac() {
+        assert!(f32::is_nan(frac(usize::MIN, usize::MIN)));
+        assert_eq!(frac(usize::MAX, usize::MAX), 1_f32);
+        assert_eq!(frac(usize::MIN, usize::MAX), 0_f32);
+        assert!(f32::is_infinite(frac(usize::MAX, usize::MIN)));
+        assert_eq!(frac(4, 1), 4_f32);
+        assert_eq!(frac(4, 2), 2_f32);
+        assert_eq!(frac(4, 3), 1.3333334_f32);
+    }
+
+    #[test]
+    fn test_frac_ceil() {
+        assert_eq!(frac_ceil(4, 1), 4_i32);
+        assert_eq!(frac_ceil(4, 2), 2_i32);
+        assert_eq!(frac_ceil(4, 3), 2_i32);
     }
 }
