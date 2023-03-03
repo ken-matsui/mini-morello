28637f62c08d6ea0aa0411e97029f654765dec24
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2023-03-03 15:36:37 -0800

    Add the 3rd dimension to spec

--- src/spec.rs
+++ src/spec.rs
@@ -2,23 +2,24 @@ use std::fmt::{Debug, Formatter};
 
 pub type Spec = MatMul;
 
-/// x > 0, y > 0
+/// x > 0, y > 0, z > 0
 #[derive(Copy, Clone, PartialEq)]
 pub struct MatMul {
     pub(crate) x: usize,
     pub(crate) y: usize,
+    pub(crate) z: usize,
 }
 
 impl Eq for MatMul {}
 
 impl MatMul {
-    pub fn new(x: usize, y: usize) -> Self {
-        Self { x, y }
+    pub fn new(x: usize, y: usize, z: usize) -> Self {
+        Self { x, y, z }
     }
 }
 
 impl Debug for MatMul {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "MatMul({}x{})", self.x, self.y)
+        write!(f, "MatMul({}x{}x{})", self.x, self.y, self.z)
     }
 }
